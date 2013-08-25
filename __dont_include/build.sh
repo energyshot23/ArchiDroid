dfc45a59a6c74ac3da694b3fe16003d811b4e247
JustArchi <archipl33@gmail.com>
2013-08-25 22:08:58 +0200

    Bump to 1.6.3 + Nova

--- __dont_include/build.sh
+++ __dont_include/build.sh
@@ -4,8 +4,8 @@
 # Not Disabled
 #exit 1
 
-WERSJA="ro.build.display.id=ArchiDroid 1.6.2 XXUFME7"
-OTA="echo \"updateme.version=1.6.2\" >> /system/build.prop"
+WERSJA="ro.build.display.id=ArchiDroid 1.6.3 XXUFME7"
+OTA="echo \"updateme.version=1.6.3\" >> /system/build.prop"
 DENSITY="#ro.sf.lcd_density=320"
 
 function zamien {
